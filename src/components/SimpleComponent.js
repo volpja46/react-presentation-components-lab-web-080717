9b5bdc3803e56a5399db6f1436e6076aa00ef3c6
Jennifer Volpe <jennifervolpe@Jennifers-MacBook-Pro.local>
2017-10-16 22:19:06 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,25 @@
 // Code SimpleComponentHere Here
+import React, { Component } from 'react';
+
+class SimpleComponentHere extends Component {
+
+
+  handleClick = () => {
+    const mood = this.state.mood === 'happy' ? 'sad' : 'happy';
+    // if mood is happy change it to sad
+    // if mood is sad change it to happy
+    this.setState({ mood});
+  }
+
+  state = {
+    mood: 'happy'
+  };
+  render(){
+      return <div onClick={this.handleClick}>{this.state.mood}</div>;
+  }
+}
+export default SimpleComponentHere;
+//
+//
+// The component should simply render its current mood state to the page in a div.
+// The component should implement a handleClick function that can serve as a callback to the <div>'s click event. When clicked, the component's mood should toggle between happy and sad states.
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,12 @@
-// Code SimplerComponentHere Here
+import React, { Component } from 'react';
+
+const simplerComponent = (props) => {
+
+  return (
+    <div onClick={props.handleClick} className = "happy">
+    return "I am just happy."
+    </div>
+  )
+}
+
+export default simplerComponent
